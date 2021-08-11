3cbb5b184540a119fd31d06f8b02578580739eac
eun9705 <38933350+eun9705@users.noreply.github.com>
2021-08-11 22:59:43 +0900

    Add files via upload

---
+++ tab.js
@@ -0,0 +1,24 @@
+var tabLink = document.getElementsByClassName('tabLink');
+var tabContent = document.getElementsByClassName('tabContent');
+
+tabLink[0].className += " active";
+tabContent[0].style.display = 'block';
+
+for(var i=0;i<tabLink.length;i++){
+	tabLink[i].addEventListener('click',function(){
+		
+		for(var j=0;j<tabLink.length;j++){
+			tabLink[j].className = 'tabLink';
+			tabContent[j].style.display = 'none';
+		}
+		
+		this.className += " active";
+		
+		var idName = this.textContent.toLowerCase();
+		
+		document.getElementById(idName).style.display = 'block';
+	});
+}
+
+
+
